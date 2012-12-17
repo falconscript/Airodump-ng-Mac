d70e57d588e2a6736952d751eeebd2601612cba0
Brandon <falconscript@gmail.com>
2012-12-17 16:35:20 -0600

    Update CookieFunctions.js

--- CookieFunctions.js
+++ CookieFunctions.js
@@ -1,14 +1,23 @@
-//Standard cookie functions. 
+/* Personal Cookie functions used to store and
+ * easily manipulate cookies safely.
+ *
+ * Also useful for debugging using Chrome inspector.
+ */
+
+//Standard cookie functions, slightly modified from w3schools.com
 function createCookie(name, uncleanValue, days)
 {
-  var value = cleanCookie(uncleanValue);
+	// Clean
+	var value = uncleanValue.toString().replace(/\t\n\;/g,'');
+	
 	if(days){
-			var date = new Date();
-			date.setTime(date.getTime()+(days*24*60*60*1000));
-			var expires = "; expires="+date.toGMTString();
-	}
-	else
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	} else {
 		var expires = "";
+	}
+		
 	document.cookie = name + "=" + value + expires + "; path=/";
 }
 
@@ -17,14 +26,14 @@ function readCookie(name)
 	var nameEQ = name + "=";
 	var cookieArray = document.cookie.split(';'); //ca is cookie array. contains all cookies
 	for(var i=0; i< cookieArray.length; i++)
-		{
-			var c = cookieArray[i]; //c is the current cookie
-			while (c.charAt(0) == ' ')
-				c = c.substring(1, c.length);
-			if(c.indexOf(nameEQ) == 0)
-				return c.substring(nameEQ.length, c.length); //from after name to end of cookie
-		}
-	//(else)
+	{
+		var c = cookieArray[i]; //c is the current cookie
+		while (c.charAt(0) == ' ')
+			c = c.substring(1, c.length);
+		if(c.indexOf(nameEQ) == 0)
+			return c.substring(nameEQ.length, c.length); //from after name to end of cookie
+	}
+	
 	return null;
 }
 
@@ -33,27 +42,6 @@ function eraseCookie(name)
 	createCookie(name,"",-1); //sets cookie to die one day ago
 }
 //End Standard Cookie Functions
-
-function cleanCookie(uncleanValue)
-{
-	uncleanValue = uncleanValue.toString();
-	
-	while ( uncleanValue.indexOf('\n') != -1 )
-		uncleanValue = uncleanValue.replace('\n','');
-		
-	while ( uncleanValue.indexOf('\t') != -1 )
-		uncleanValue = uncleanValue.replace('\t','');
-	
-	if (uncleanValue.indexOf(';') != -1 )
-	{
-		//alert("You are trying to put semicolons in a cookie! Think about it!");
-	}
-	
-	while ( uncleanValue.indexOf(';') != -1 )
-		uncleanValue = uncleanValue.replace(';','');
-	
-	return uncleanValue;
-}
 
 function createCookieArray(name, arrayValues, days)
 {
@@ -89,21 +77,22 @@ function eraseAllCookies()
 
 function cookieExists(name)
 {
-	if( readCookie(name) != null && readCookie(name) != "" )
-		return true;
-	else
-		return false;
+	return readCookie(name) != null && readCookie(name) != "";
 }
 
-function isUndefined(x) {
+function isUndefined(x)
+{
 	return x == null && x !== null;
 }
 
 function setFullCookieFromString(s) {
-	var cooks = s.split('; ');
+	var cooks = s.split(';'); // May sometimes need '; ' instead
+	
+	// Iterate through cookies
 	for(var i in cooks) {
 	
-		if(typeof cooks[i] != "string") continue; // ie protect
+		if(typeof cooks[i] != "string")  // ie protect
+			continue;
 		
 		createCookie(cooks[i].split('=')[0], cooks[i].split('=')[1],365);
 	}
